a76277cb0afd2de49a90612dc51afcaaf6a7dc23
alvaroUbuntu <alvaro.mho2@gmail.com>
2015-05-02 22:17:09 -0300

    Hola

---
+++ app/src/main/java/com/example/alvaro/padrinounab/Menu.java
@@ -0,0 +1,20 @@
+package com.example.alvaro.padrinounab;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+
+/**
+ * Created by alvaro on 02-05-15.
+ */
+public class Menu  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+
+
+    }
+}
